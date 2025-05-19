f2c9b05b1c9202ad12c85c59d353f583039af931
vanshbhandari31 <vanshbhandari2004@gmail.com>
2025-05-20 00:16:34 +0530

    solved

--- 49_groupanagram.py
+++ 49_groupanagram.py
@@ -1,32 +1,17 @@
 class Solution(object):
-    def isanagram(self,s1,s2):
-        if len(s1)!=len(s2):
-            return False
-        count1=[0]*26
-        count2=[0]*26
-        for i in s1:
-            count1[ord(i)-ord('a')]+=1
-        for i in s2:
-            count2[ord(i)-ord('a')]+=1
-        return count1==count2
+    def getKey(self, word):
+        count = [0] * 26
+        for ch in word:
+            count[ord(ch) - ord('a')] += 1
+        return tuple(count)
 
     def groupAnagrams(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: List[List[str]]
-        """
-        res=[]
-        visited=[False]*len(strs)
-        for i in range(len(strs)):
-            if not visited[i]:
-                group=[strs[i]]
-                visited[i]=True
+        hash_map = {}
 
-                for j in range(i+1,len(strs)):
-                    if not visited[j] and self.isanagram(strs[i],strs[j]):
-                        group.append(strs[j])
-                        visited[j]=True
-                res.append(group)
-        return res
+        for word in strs:
+            key = self.getKey(word)
+            if key not in hash_map:
+                hash_map[key] = []
+            hash_map[key].append(word)
 
-
+        return list(hash_map.values())
