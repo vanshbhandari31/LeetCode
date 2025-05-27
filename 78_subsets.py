d86079d835c4b5cd22dda0168ae02df98da52c56
vanshbhandari31 <vanshbhandari2004@gmail.com>
2025-05-27 12:29:03 +0530

    solved

---
+++ 78_subsets.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def subsets(self, nums):
+        ans = []
+        curr = []
+
+        def dfs(i):
+            if i >= len(nums):
+                ans.append(list(curr))
+                return
+            curr.append(nums[i])
+            dfs(i + 1)
+            curr.pop()
+            dfs(i + 1)
+
+        dfs(0)
+        return ans
