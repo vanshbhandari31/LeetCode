67473b6e537152151468f318ed99e998bf72c0b5
vanshbhandari31 <vanshbhandari2004@gmail.com>
2025-05-27 10:04:19 +0530

    solved

--- 2894_divisibleNondivisible.py
+++ 2894_divisibleNondivisible.py
@@ -6,12 +6,10 @@ class Solution(object):
         :rtype: int
         """
         num1=0
+        num2=0
         for i in range(1,n+1):
             if i%m!=0:
                 num1=num1+i
-        num2=0
-        for i in range(1,n+1):
-            if i%m==0:
+            else:
                 num2=num2+i
-
         return num1-num2
