aa57fda38691d00c6be3b029274ee93404ee88b0
vanshbhandari31 <vanshbhandari2004@gmail.com>
2025-05-27 10:01:16 +0530

    solved

---
+++ 2894_divisibleNondivisible.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def differenceOfSums(self, n, m):
+        """
+        :type n: int
+        :type m: int
+        :rtype: int
+        """
+        num1=0
+        for i in range(1,n+1):
+            if i%m!=0:
+                num1=num1+i
+        num2=0
+        for i in range(1,n+1):
+            if i%m==0:
+                num2=num2+i
+
+        return num1-num2
