4dc624a004d3e70792205b4f3ed973412b1e211e
vanshbhandari31 <vanshbhandari2004@gmail.com>
2025-08-14 02:49:33 +0530

    SOLVED

---
+++ 326_powerOfThree.py
@@ -0,0 +1,19 @@
+import math
+
+from PIL.SpiderImagePlugin import isInt
+
+
+class Solution(object):
+    def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n<=0:
+            return False
+        while n%3==0:
+            n=n/3
+        return n==1
+
+
+
